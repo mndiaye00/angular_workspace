2f66f1a4e1dbd89e69adcc4ee4db10bf0519af0f
mndiaye00 <msanes13@gmail.com>
2020-03-03 20:07:39 -0500

    Modification du fichier app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Pokemon } from './pokemon';
+import { OnInit } from "@angular/core";
 
 @Component ({
     selector: 'pokemon-app',
     template: `<h1>Hello {{ name }}</h1>`
 })
 
-export class AppComponent{
+export class AppComponent implements OnInit{
     name = "Moussa";
+
+    ngOnInit(){
+
+    }
 
     private pokemons : Pokemon[];
 
